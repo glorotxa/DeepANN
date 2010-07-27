c576bd47ee5e4882dfeff7b9209ab419dade0fea
Xavier Glorot <glorotxa@iro.umontreal.ca>
2010-07-27 17:21:51 -0400

    bug on GPU memmory

--- exp_scripts/testNLP.py
+++ exp_scripts/testNLP.py
@@ -1,10 +1,7 @@
-from deepANN.ANN import *
+from ANN import *
 import cPickle
-import copy, os
-from pylearn.io import filetensor
+import os
 import time
-
-import pylearn.datasets.MNIST
 
 lr = 0.01
 act='tanh'
@@ -13,8 +10,8 @@ n_hid = 1400
 noise = (0.7,0.0041)
 l1=0.0
 l2=0.0
-nepochs = 20
-batchsize = 16
+nepochs = 30
+batchsize = 10
 
 
 
@@ -31,21 +28,38 @@ f.close()
 #trainl = T.shared(dat.train.y,name = 'trainl')
 
 
-model=SDAE(numpy.random,RandomStreams(),depth,True,act=act,n_hid=n_hid,n_out=5,n_inp=5000,noise='binomial_NLP')
+model=SDAE(numpy.random,RandomStreams(),depth,True,act=act,n_hid=n_hid,n_out=5,sparsity=l1,n_inp=5000,noise='binomial_NLP')
 model.auxiliary(1,0,5000)
 model.ModeAux(depth,update_type='special',noise_lvl=noise,lr=lr)
 
 
 g1,n = model.trainfunctionbatch(train,trainl,train, batchsize=batchsize)
-tes = model.costfunction(train,trainl,train, batchsize=1000)
+tes = model.costfunction(train,trainl,train, batchsize=100)
+
+print tes()
 
 for cc in range(nepochs):
-    for p in xrange(1,16):
+    time1 = time.time()
+    for p in xrange(14,16):
+        time2 = time.time()
         f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_train_%s.pkl'%p,'r')
         train.value = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
+        f.close()
         f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_trainl_%s.pkl'%p,'r')
         trainl.value = cPickle.load(f)
-        for i in range(n):
-            time1 = time.time()
-            print g1(i),i,time.time()-time1
-        print tes()
+        f.close()
+        for i in range(train.value.shape[0]/batchsize):
+            dum = g1(i)
+        print 'file',p,'time',time.time()-time2
+        #if p==14:
+        #    print tes() #memmory-bug
+    f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_train_1.pkl','r')
+    train.value = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
+    f.close()
+    f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_trainl_1.pkl','r')
+    trainl.value = cPickle.load(f)
+    f.close()
+    print tes()
+    print '-----------------------------epoch',cc,'time',time.time()-time1
+
+model.save('/u/glorotxa/depth1ronan/')
