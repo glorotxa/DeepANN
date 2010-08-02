cec24f0b68c4bf8a6b22223843a6d9f340ab0a74
Xavier Glorot <glorotxa@iro.umontreal.ca>
2010-08-02 17:33:05 -0400

    new NLP script

--- exp_scripts/testNLP.py
+++ exp_scripts/testNLP.py
@@ -6,15 +6,16 @@ import cPickle
 import os
 import time
 
-lr = 0.01
-act='tanh'
+lr = 0.0001
+act=['tanh']
 depth=1
-n_hid = 1400
+n_hid = [1400]
 noise = (0.7,0.0041)
 l1=0.0
 l2=0.0
 nepochs = 30
 batchsize = 10
+path2savemodel = '/u/glorotxa/'
 
 
 
@@ -26,47 +27,59 @@ f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_trainl_1.pkl','r')
 trainl = theano.shared(cPickle.load(f),name = 'trainl')
 f.close()
 
-#dat = pylearn.datasets.MNIST.full()
-#train = T.shared(numpy.asarray(dat.train.x,dtype=theano.config.floatX))
-#trainl = T.shared(dat.train.y,name = 'trainl')
 
-
-model=SDAE(numpy.random,RandomStreams(),depth,True,act=act,n_hid=n_hid,n_out=5,sparsity=l1,n_inp=5000,noise='binomial_NLP')
-model.auxiliary(1,0,5000)
+model=SDAE(numpy.random,RandomStreams(),depth,True,act=act,n_hid=n_hid,n_out=5,sparsity=l1,n_inp=5000,noise='binomial',tie=False)
+model.auxiliary(1,0,2500)
 model.ModeAux(depth,update_type='special',noise_lvl=noise,lr=lr)
 
+#model.layers[0].W.value = cPickle.load(open('/u/glorotxa/depth2ronan/Layer1_W.pkl','r'))
+#model.layers[0].b.value = cPickle.load(open('/u/glorotxa/depth2ronan/Layer1_b.pkl','r'))
+#model.layers[0].mask.value = cPickle.load(open('/u/glorotxa/depth2ronan/Layer1_mask.pkl','r'))
+#model.layers[1].W.value = cPickle.load(open('/u/glorotxa/depth2ronan/Layer2_W.pkl','r'))
+#model.layers[1].b.value = cPickle.load(open('/u/glorotxa/depth2ronan/Layer2_b.pkl','r'))
+#model.layers[1].mask.value = cPickle.load(open('/u/glorotxa/depth2ronan/Layer2_mask.pkl','r'))
 
-g1,n = model.trainfunctionbatch(train,trainl,train, batchsize=batchsize)
+#givens = {}
+#index = T.lscalar()
+#givens.update({model.inp : train[index*batchsize:(index+1)*batchsize]})
+#givens.update({model.auxtarget : (model.layers[1].out+1.)/2.})
+#g1 = theano.function([index],[model.cost],updates = model.updates, givens = givens)
+
+#def tes():
+#    sum=0
+#    for i in range(train.value.shape[0]/100):
+#        sum+=g1(i)[0]
+#    return sum/float(i+1)
+
+g1,n = model.trainfunctionbatch(train,trainl,train , batchsize=batchsize)
 tes = model.costfunction(train,trainl,train, batchsize=100)
 
-STRANGE_MEMORY_TMP_FIX = False
-
-if STRANGE_MEMORY_TMP_FIX:
-    train_gpu_ptr = train.container.storage[0]
+normalshape = train.value.shape
 
 print tes()
 
 for cc in range(nepochs):
     time1 = time.time()
-    for p in xrange(14,16):
+    for p in xrange(1,16):
         time2 = time.time()
         f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_train_%s.pkl'%p,'r')
-        if STRANGE_MEMORY_TMP_FIX:
-            data = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
-            train_gpu_ptr[0:data.shape[0],:] = data
-            train.container.storage[0] = train_gpu_ptr[0:data.shape[0],:]
-            assert train.container.storage[0].shape == data.shape
+        object = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
+        if object.shape == normalshape:
+            train.value = object
+            currentn = normalshape[0]
+            del object
         else:
-            train.value = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
+            train.value = numpy.concatenate([object,\
+                    numpy.zeros((normalshape[0]-object.shape[0],normalshape[1]),dtype=theano.config.floatX)])
+            currentn = object.shape[0]
+            del object
         f.close()
         f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_trainl_%s.pkl'%p,'r')
         trainl.value = cPickle.load(f)
         f.close()
-        for i in range(train.value.shape[0]/batchsize):
+        for i in range(currentn/batchsize):
             dum = g1(i)
         print 'file',p,'time',time.time()-time2
-        #if p==14:
-        #    print tes() #memmory-bug
     f =open('/u/glorotxa/work/NLP/DARPAproject/NEC/OpenTable5000_train_1.pkl','r')
     train.value = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
     f.close()
@@ -76,4 +89,4 @@ for cc in range(nepochs):
     print tes()
     print '-----------------------------epoch',cc,'time',time.time()-time1
 
-model.save('/u/glorotxa/depth1ronan/')
+#model.save(path2save)
