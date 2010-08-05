a5113e2ba79a4c89fdc156c4daf09a141e626a5a
Joseph Turian <turian@gmail.com>
2010-08-05 14:28:32 -0400

    Refactor randomtransformation.py code

--- exp_scripts/randomprojection.py
+++ exp_scripts/randomprojection.py
@@ -37,11 +37,77 @@ MODE = "online"     # Deterministic, low-memory version
                     # Could be, in principle, slower than online if the
                     # number of entries >> #nonzeros.
 
+RANDOMIZATION_TYPE="gaussian"
+                    # No other option is currently supported
+
 if MODE == "online":
     import pyrandomprojection
+elif MODE == "batch":
+    # Cache the random matrix in memory
+    randommatrix = None
 
 
 #def project(dict, dimensions, RANDOMIZATION_TYPE="gaussian", TERNARY_NON_ZERO_PERCENT=None, RANDOM_SEED=0):
+
+def batchproject(x, dimensions, seed, randomization_type):
+    # Batch (cached, high-memory) random projection
+    global randommatrix
+
+    if randommatrix is None:
+        print >> sys.stderr, "Creating random matrix of shape %s" % `(x.shape[1], dimensions)`
+        print >> sys.stderr, stats()
+        numpy.random.seed(seed)
+        assert randomization_type == "gaussian"
+        randommatrix = numpy.random.normal(size=(x.shape[1], dimensions))
+    else:
+        assert randommatrix.shape == (x.shape[1], dimensions)       # We assume the projection matrix won't change
+
+    print >> sys.stderr, "Multiplying x by random matrix..."
+    print >> sys.stderr, stats()
+    newx = numpy.dot(x, randommatrix)
+    print >> sys.stderr, "...done multiplying x by random matrix"
+    print >> sys.stderr, stats()
+    
+    return newx
+
+
+def onlineproject(x, dimensions, seed, randomization_type):
+    # Online (low-memory) random projection
+    
+    newx = numpy.zeros((x.shape[0], dimensions))
+
+    nonzeros = x.nonzero()      # (list of rows, list of cols) of all nonzeros
+    
+    # (col, row) of all nonzeros
+    # We reorder like this so that we can group all columns together, and look up the randomrow for each column feature only once.
+    nonzero_colrow = [(nonzeros[1][l], nonzeros[0][l]) for l in range(len(nonzeros[0]))]
+    nonzero_colrow.sort()
+    nonzero_colrow.reverse()
+    
+    randrow_key = None
+    randrow_values = None
+    randrows_computed = 0
+    for l, (col, row) in enumerate(nonzero_colrow):
+        if randrow_key != col:
+            randrow_key = col
+            randrow_values = pyrandomprojection.randomrow(key=col, dimensions=dimensions, RANDOMIZATION_TYPE=randomization_type, RANDOM_SEED=seed)
+
+            randrows_computed += 1
+            if randrows_computed % 100 == 0:
+                print >> sys.stderr, "Retrieved %s random rows thus far, done with %s of nonzeroes on %s..." % (percent(randrows_computed, x.shape[1]), percent(l+1, len(nonzero_colrow)), f)
+                print >> sys.stderr, stats()
+        newrow = x[row,col] * randrow_values
+        assert newx[row].shape == newrow.shape
+        newx[row] += newrow
+#        if (l+1) % 10000 == 0:
+#            print >> sys.stderr, "Done with %s of nonzeroes on %s..." % (percent(l+1, len(nonzero_colrow)), f)
+#            print >> sys.stderr, stats()
+    return newx
+
+def project(x, dimensions, seed, randomization_type, mode=MODE):
+    if mode == "batch": return batchproject(x, dimensions, seed, randomization_type)
+    elif mode == "online": return onlineproject(x, dimensions, seed, randomization_type)
+    else: assert 0
 
 if __name__ == "__main__":
     from optparse import OptionParser
@@ -56,10 +122,6 @@ if __name__ == "__main__":
 
     assert len(args) > 0    # You need to pass in pkl files to project.
 
-    if MODE == "batch":
-        # Cache the random matrix in memory
-        randommatrix = None
-
     for i, f in enumerate(args):
         print >> sys.stderr, "\nLoading %s (file %s)..." % (f, percent(i+1, len(args)))
         print >> sys.stderr, stats()
@@ -68,53 +130,7 @@ if __name__ == "__main__":
         print >> sys.stderr, stats()
 
         assert x.ndim == 2
-        newx = numpy.zeros((x.shape[0], options.dimensions))
-        print >> sys.stderr, "Read instance matrix with shape %s, creating projection with shape %s" % (x.shape, newx.shape)
+        print >> sys.stderr, "Read instance matrix with shape %s, creating projection with shape %s" % (x.shape, (x.shape[0], options.dimensions))
 
-        if MODE == "batch":
-            # Batch (cached, high-memory) random projection
-            if randommatrix is None:
-                print >> sys.stderr, "Creating random matrix of shape %s" % `(x.shape[1], options.dimensions)`
-                print >> sys.stderr, stats()
-                numpy.random.seed(options.seed)
-                randommatrix = numpy.random.normal(size=(x.shape[1], options.dimensions))
-            else:
-                assert randommatrix.shape == (x.shape[1], options.dimensions)       # We assume the projection matrix won't change
-
-            print >> sys.stderr, "Multiplying x by random matrix..."
-            print >> sys.stderr, stats()
-            newx = numpy.dot(x, randommatrix)
-            print >> sys.stderr, "...done multiplying x by random matrix"
-            print >> sys.stderr, stats()
-
-        elif MODE == "online":
-            # Online (low-memory) random projection
-            nonzeros = x.nonzero()      # (list of rows, list of cols) of all nonzeros
-    
-            # (col, row) of all nonzeros
-            # We reorder like this so that we can group all columns together, and look up the randomrow for each column feature only once.
-            nonzero_colrow = [(nonzeros[1][l], nonzeros[0][l]) for l in range(len(nonzeros[0]))]
-            nonzero_colrow.sort()
-            nonzero_colrow.reverse()
-    
-            randrow_key = None
-            randrow_values = None
-            randrows_computed = 0
-            for l, (col, row) in enumerate(nonzero_colrow):
-                if randrow_key != col:
-                    randrow_key = col
-                    randrow_values = pyrandomprojection.randomrow(key=col, dimensions=options.dimensions, RANDOM_SEED=options.seed)
-
-                    randrows_computed += 1
-                    if randrows_computed % 100 == 0:
-                        print >> sys.stderr, "Retrieved %s random rows thus far, done with %s of nonzeroes on %s..." % (percent(randrows_computed, x.shape[1]), percent(l+1, len(nonzero_colrow)), f)
-                        print >> sys.stderr, stats()
-                newrow = x[row,col] * randrow_values
-                assert newx[row].shape == newrow.shape
-                newx[row] += newrow
-    
-#                if (l+1) % 10000 == 0:
-#                    print >> sys.stderr, "Done with %s of nonzeroes on %s..." % (percent(l+1, len(nonzero_colrow)), f)
-#                    print >> sys.stderr, stats()
-
-        else: assert 0
+        newx = project(x, dimensions=options.dimensions, seed=options.seed, randomization_type=RANDOMIZATION_TYPE, mode=MODE)
+        assert newx.shape == (x.shape[0], options.dimensions)
