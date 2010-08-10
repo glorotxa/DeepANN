ed89f878db60659bb8fc7de35aa6bd6bf7f82a37
Joseph Turian <turian@gmail.com>
2010-08-10 15:06:55 -0400

    Removed hardcoded usage of validation_runs_for_each_trainingsize

--- exp_scripts/DARPAscript.py
+++ exp_scripts/DARPAscript.py
@@ -14,7 +14,7 @@ from jobman.parse import filemerge
 from common.stats import stats
 
 #hardcoded path to your liblinear source:
-SVMPATH = '/u/glorotxa/work/NLP/DARPAproject/netscale_sentiment_for_ET/lib/liblinear/' 
+SVMPATH = '/u/glorotxa/work/NLP/DARPAproject/netscale_sentiment_for_ET/lib/liblinear/'
 SVMRUNALL_PATH = os.path.join(SVMPATH, "run_all")
 assert os.access(SVMRUNALL_PATH, os.X_OK)
 
@@ -36,11 +36,13 @@ def rebuildunsup(model,depth,ACT,LR,NOISE_LVL,batchsize,train):
         n = train.value.shape[0] / batchsize
         def tes():
             sum=0
+            # TODO: What is this magic number 100?
             for i in range(train.value.shape[0]/100):
                 sum+=testfunc(i)
             return sum/float(i+1)
     else:
         trainfunc,n = model.trainfunctionbatch(train,None,train, batchsize=batchsize)
+        # TODO: What is this magic number 100?
         tes = model.costfunction(train,None,train, batchsize=100)
     return trainfunc,n,tes
 
@@ -56,6 +58,7 @@ def createlibsvmfile(model,depth,datafiles,dataout):
     labels = numpy.asarray(cPickle.load(f),dtype = 'int64')
     f.close()
     f = open(dataout,'w')
+    # TODO: What is this magic number 10000 and 500?
     for i in range(10000/500):
         textr = ''
         rep = func(instances[500*i:500*(i+1),:])[0]
@@ -71,18 +74,14 @@ def createlibsvmfile(model,depth,datafiles,dataout):
     print >> sys.stderr, "...done creating libsvm files"
     print >> sys.stderr, stats()
 
-def dosvm(nbinputs,datatrainsave,datatestsave,PATH_SAVE):
-    if nbinputs == 100:
-        NUMRUNS = 20
-    elif nbinputs == 1000:
-        NUMRUNS = 10
-    elif nbinputs == 10000:
-        NUMRUNS = 1
+def dosvm(nbinputs,numruns,datatrainsave,datatestsave,PATH_SAVE):
+    # TODO: Rewrite with a proper linesearch
+
     print >> sys.stderr, 'BEGIN SVM for %s examples'%nbinputs
     C = 0.01
     print >> sys.stderr, C , '-----------------------------------------------------------------'
     print >> sys.stderr, stats()
-    os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,NUMRUNS,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
+    os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,numruns,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
     f = open(PATH_SAVE+'/currentsvm.txt','r')
     a=f.readline()[:-1]
     f.close()
@@ -93,11 +92,11 @@ def dosvm(nbinputs,datatrainsave,datatestsave,PATH_SAVE):
     testerr = [float(res[3])]
     testerrdev = [float(res[4])]
     Clist=[C]
-    
+
     C = 0.001
     print >> sys.stderr, C  , '-----------------------------------------------------------------'
     print >> sys.stderr, stats()
-    os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,NUMRUNS,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
+    os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,numruns,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
     f = open(PATH_SAVE+'/currentsvm.txt','r')
     a=f.readline()[:-1]
     f.close()
@@ -108,13 +107,13 @@ def dosvm(nbinputs,datatrainsave,datatestsave,PATH_SAVE):
     testerr = [float(res[3])] + testerr
     testerrdev = [float(res[4])] + testerrdev
     Clist=[C] + Clist
-    
+
     if testerr[1] < testerr[0]:
-        C = 0.1        
+        C = 0.1
         while testerr[-1] < testerr[-2] and C<100000:
             print >> sys.stderr, C , '-----------------------------------------------------------------'
             print >> sys.stderr, stats()
-            os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,NUMRUNS,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
+            os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,numruns,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
             f = open(PATH_SAVE+'/currentsvm.txt','r')
             a=f.readline()[:-1]
             f.close()
@@ -135,7 +134,7 @@ def dosvm(nbinputs,datatrainsave,datatestsave,PATH_SAVE):
         while testerr[0] < testerr[1] and C>0.000001:
             print >> sys.stderr, C , '-----------------------------------------------------------------'
             print >> sys.stderr, stats()
-            os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,NUMRUNS,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
+            os.system('%s -s 4 -c %s -l %s -r %s -q %s %s %s'%(SVMRUNALL_PATH,C,nbinputs,numruns,datatrainsave,datatestsave,PATH_SAVE+'/currentsvm.txt'))
             f = open(PATH_SAVE+'/currentsvm.txt','r')
             a=f.readline()[:-1]
             f.close()
@@ -152,13 +151,13 @@ def dosvm(nbinputs,datatrainsave,datatestsave,PATH_SAVE):
         else:
             return Clist[0],testerr[0],testerrdev[0],trainerr[0],trainerrdev[0]
 
-    
+
 def NLPSDAE(state,channel):
     """This script launch a new, or stack on previous, SDAE experiment, training in a greedy layer wise fashion.
     Only tanh and sigmoid activation are supported for stacking, a rectifier activation is possible at the last layer.
     (waiting to validate the best method to stack rectifier on NISTP), it is possible to give a tfidf representation,
     it will then create a softplus auxlayer for the depth 1 unsupervised pre-training with a quadratic reconstruction cost"""
-    
+
     # Hyper-parameters
     LR = state.lr#list
     ACT = state.act #list
@@ -171,8 +170,9 @@ def NLPSDAE(state,channel):
     WEIGHT_REGULARIZATION_TYPE = state.weight_regularization_type
     WEIGHT_REGULARIZATION_COEFF = state.weight_regularization_coeff #list
     NEPOCHS = state.nepochs #list
-    VALIDATION_RUNS_FOR_EACH_TRAININGSIZE = state.validation_runs_for_each_trainingsize #list of lists
-    VALIDATION_TRAININGSIZE = [i[0] for i in VALIDATION_RUNS_FOR_EACH_TRAININGSIZE] # list
+    VALIDATION_RUNS_FOR_EACH_TRAININGSIZE = state.validation_runs_for_each_trainingsize #dict from trainsize string to number of validation runs at this training size
+    VALIDATION_TRAININGSIZE = [int(trainsize) for trainsize in VALIDATION_RUNS_FOR_EACH_TRAININGSIZE] # list
+    VALIDATION_TRAININGSIZE.sort()
     EPOCHSTEST = state.epochstest #list
     BATCHSIZE = state.batchsize
     PATH_SAVE = channel.remote_path if hasattr(channel,'remote_path') else channel.path
@@ -187,20 +187,20 @@ def NLPSDAE(state,channel):
     INPUTTYPE = state.inputtype
     RandomStreams(state.seed)
     numpy.random.seed(state.seed)
-    
+
     datatrain = (PATH_DATA+NAME_DATA+'_1.pkl',PATH_DATA+NAME_LABEL+'_1.pkl')
     datatrainsave = PATH_SAVE+'/train.libsvm'
     datatest = (PATH_DATA+NAME_DATATEST+'_1.pkl',PATH_DATA+NAME_LABELTEST+'_1.pkl')
     datatestsave = PATH_SAVE+'/test.libsvm'
-    
+
     depthbegin = 0
-    
+
     #monitor best performance for reconstruction and classification
     state.bestrec = []
     state.bestrecepoch = []
-    state.besterr = dict([(trainsize, []) for trainsize in VALIDATION_TRAININGSIZE])
-    state.besterrepoch = dict([(trainsize, []) for trainsize in VALIDATION_TRAININGSIZE])
-    
+    state.besterr = dict([(`trainsize`, []) for trainsize in VALIDATION_TRAININGSIZE])
+    state.besterrepoch = dict([(`trainsize`, []) for trainsize in VALIDATION_TRAININGSIZE])
+
     if MODEL_RELOAD != None:
         oldstate = expand(DD(filemerge(MODEL_RELOAD+'../current.conf')))
         DEPTH = oldstate.depth + DEPTH
@@ -219,17 +219,17 @@ def NLPSDAE(state,channel):
         del oldstate
 
     if 'rectifier' in ACT:
-        assert ACT.index('rectifier')== DEPTH -1 
+        assert ACT.index('rectifier')== DEPTH -1
         # Methods to stack rectifier are still in evaluation (5 different techniques)
         # The best will be implemented in the script soon :).
     f =open(PATH_DATA + NAME_DATATEST + '_1.pkl','r')
     train = theano.shared(numpy.asarray(cPickle.load(f),dtype=theano.config.floatX))
     f.close()
     normalshape = train.value.shape
-   
+
     model=SDAE(numpy.random,RandomStreams(),DEPTH,True,act=ACT,n_hid=N_HID,n_out=5,sparsity=ACTIVATION_REGULARIZATION_COEFF,\
             regularization=WEIGHT_REGULARIZATION_COEFF, wdreg = WEIGHT_REGULARIZATION_TYPE, spreg = ACTIVATION_REGULARIZATION_TYPE, n_inp=NINPUTS,noise=NOISE,tie=True)
-    
+
     #RELOAD previous model
     for i in range(depthbegin):
         print >> sys.stderr, 'reload layer',i+1
@@ -237,7 +237,7 @@ def NLPSDAE(state,channel):
         model.layers[i].W.value = cPickle.load(open(MODEL_RELOAD + 'Layer%s_W.pkl'%(i+1),'r'))
         model.layers[i].b.value = cPickle.load(open(MODEL_RELOAD + 'Layer%s_b.pkl'%(i+1),'r'))
         model.layers[i].mask.value = cPickle.load(open(MODEL_RELOAD + 'Layer%s_mask.pkl'%(i+1),'r'))
-    
+
     state.act = ACT
     state.depth = DEPTH
     state.depthbegin = depthbegin
@@ -250,7 +250,7 @@ def NLPSDAE(state,channel):
     state.noise_lvl = NOISE_LVL
     state.epochstest = EPOCHSTEST
     channel.save()
-    
+
     for i in xrange(depthbegin,DEPTH):
         print >> sys.stderr, '-----------------------------BEGIN DEPTH:',i+1
         print >> sys.stderr, stats()
@@ -271,32 +271,29 @@ def NLPSDAE(state,channel):
                 del model.auxlayer.W
                 del model.auxlayer.b
             model.auxiliary(init=1,auxdepth=-DEPTH+i+1, auxn_out=n_aux)
-        
+
         rec = {}
         err = dict([(trainsize, {}) for trainsize in VALIDATION_TRAININGSIZE])
-        
-        if 0 in EPOCHSTEST[i]: 
+
+        if 0 in EPOCHSTEST[i]:
             trainfunc,n,tes = rebuildunsup(model,i,ACT,LR[i],None,BATCHSIZE,train)
             createlibsvmfile(model,i,datatrain,datatrainsave)
             createlibsvmfile(model,i,datatest,datatestsave)
-            C,testerr,testerrdev,trainerr,trainerrdev = dosvm(100,datatrainsave,datatestsave,PATH_SAVE)
-            err100.update({0:(C,testerr,testerrdev,trainerr,trainerrdev)})
-            C,testerr,testerrdev,trainerr,trainerrdev = dosvm(1000,datatrainsave,datatestsave,PATH_SAVE)
-            err1000.update({0:(C,testerr,testerrdev,trainerr,trainerrdev)})
-            C,testerr,testerrdev,trainerr,trainerrdev = dosvm(10000,datatrainsave,datatestsave,PATH_SAVE)
-            err10000.update({0:(C,testerr,testerrdev,trainerr,trainerrdev)})
-        
+
+            for trainsize in VALIDATION_TRAININGSIZE:
+                C,testerr,testerrdev,trainerr,trainerrdev = dosvm(trainsize,VALIDATION_RUNS_FOR_EACH_TRAININGSIZE[`trainsize`],datatrainsave,datatestsave,PATH_SAVE)
+                err[trainsize].update({0:(C,testerr,testerrdev,trainerr,trainerrdev)})
+
         trainfunc,n,tes = rebuildunsup(model,i,ACT,LR[i],NOISE_LVL[i],BATCHSIZE,train)
         if 0 in EPOCHSTEST[i]:
             rec.update({0:tes()})
-            
+
             print >> sys.stderr, '########## INITIAL TEST ############  : '
             print >> sys.stderr, 'CURRENT RECONSTRUCTION ERROR: ',rec[0]
-            print >> sys.stderr, 'CURRENT 100 SVM ERROR: ',err100[0]
-            print >> sys.stderr, 'CURRENT 1000 SVM ERROR: ',err1000[0]
-            print >> sys.stderr, 'CURRENT 10000 SVM ERROR: ',err10000[0]
+            for trainsize in VALIDATION_TRAININGSIZE:
+                print >> sys.stderr, 'CURRENT %d SVM ERROR: ' % trainsize, err[trainsize][0]
             print >> sys.stderr, stats()
-            
+
         for cc in range(NEPOCHS[i]):
             time1 = time.time()
             for p in xrange(1,NB_FILES + 1):
@@ -326,30 +323,28 @@ def NLPSDAE(state,channel):
             if cc+1 in EPOCHSTEST[i]:
                 trainfunc,n,tes = rebuildunsup(model,i,ACT,LR[i],None,BATCHSIZE,train)
                 createlibsvmfile(model,i,datatrain,datatrainsave)
-                createlibsvmfile(model,i,datatest,datatestsave)                
-                C,testerr,testerrdev,trainerr,trainerrdev = dosvm(100,datatrainsave,datatestsave,PATH_SAVE)
-                err100.update({cc+1:(C,testerr,testerrdev,trainerr,trainerrdev)})
-                C,testerr,testerrdev,trainerr,trainerrdev = dosvm(1000,datatrainsave,datatestsave,PATH_SAVE)
-                err1000.update({cc+1:(C,testerr,testerrdev,trainerr,trainerrdev)})
-                C,testerr,testerrdev,trainerr,trainerrdev = dosvm(10000,datatrainsave,datatestsave,PATH_SAVE)
-                err10000.update({cc+1:(C,testerr,testerrdev,trainerr,trainerrdev)})
-                
+                createlibsvmfile(model,i,datatest,datatestsave)
+
+                # TODO: Dedup this code with above copy
+                for trainsize in VALIDATION_TRAININGSIZE:
+                    C,testerr,testerrdev,trainerr,trainerrdev = dosvm(trainsize,VALIDATION_RUNS_FOR_EACH_TRAININGSIZE[`trainsize`],datatrainsave,datatestsave,PATH_SAVE)
+                    err[trainsize].update({cc+1:(C,testerr,testerrdev,trainerr,trainerrdev)})
+
                 trainfunc,n,tes = rebuildunsup(model,i,ACT,LR[i],NOISE_LVL[i],BATCHSIZE,train)
                 f =open(PATH_DATA + NAME_DATATEST +'_1.pkl','r')
                 train.value = numpy.asarray(cPickle.load(f),dtype=theano.config.floatX)
                 f.close()
                 rec.update({cc+1:tes()})
+                # TODO: Dedup this code with above copy
                 print >> sys.stderr, '##########  TEST ############ EPOCH : ',cc+1
                 print >> sys.stderr, 'CURRENT RECONSTRUCTION ERROR: ',rec[cc+1]
-                print >> sys.stderr, 'CURRENT 100 SVM ERROR: ',err100[cc+1]
-                print >> sys.stderr, 'CURRENT 1000 SVM ERROR: ',err1000[cc+1]
-                print >> sys.stderr, 'CURRENT 10000 SVM ERROR: ',err10000[cc+1]
+                for trainsize in VALIDATION_TRAININGSIZE:
+                    print >> sys.stderr, 'CURRENT %d SVM ERROR: ' % trainsize,err[trainsize][cc+1]
                 print >> sys.stderr, stats()
                 f = open('depth%serr.pkl'%i,'w')
                 cPickle.dump(rec,f,-1)
-                cPickle.dump(err100,f,-1)
-                cPickle.dump(err1000,f,-1)
-                cPickle.dump(err10000,f,-1)
+                for trainsize in VALIDATION_TRAININGSIZE:
+                    cPickle.dump(err[trainsize],f,-1)
                 f.close()
                 os.mkdir(PATH_SAVE+'/depth%spre%s'%(i+1,cc+1))
                 model.save(PATH_SAVE+'/depth%spre%s'%(i+1,cc+1))
@@ -359,40 +354,20 @@ def NLPSDAE(state,channel):
                 if rec[k] == recmin:
                     state.bestrec += [recmin]
                     state.bestrecepoch += [k]
-            
-            errvector = err100.values()
-            for k in range(len(errvector)):
-                errvector[k] = errvector[k][1]
-            errmin = numpy.min(errvector)
-            for k in err100.keys():
-                if err100[k][1] == errmin:
-                    state.besterr100 += [err100[k]]
-                    state.besterr100epoch += [k]
-            
-            errvector = err1000.values()
-            for k in range(len(errvector)):
-                errvector[k] = errvector[k][1]
-            errmin = numpy.min(errvector)
-            for k in err1000.keys():
-                if err1000[k][1] == errmin:
-                    state.besterr1000 += [err1000[k]]
-                    state.besterr1000epoch += [k]
-            
-            errvector = err10000.values()
-            for k in range(len(errvector)):
-                errvector[k] = errvector[k][1]
-            errmin = numpy.min(errvector)
-            for k in err10000.keys():
-                if err10000[k][1] == errmin:
-                    state.besterr10000 += [err10000[k]]
-                    state.besterr10000epoch += [k]
+
+            for trainsize in VALIDATION_TRAININGSIZE:
+                errvector = err[trainsize].values()
+                for k in range(len(errvector)):
+                    errvector[k] = errvector[k][1]
+                errmin = numpy.min(errvector)
+                for k in err[trainsize].keys():
+                    if err[trainsize][k][1] == errmin:
+                        state.besterr[trainsize] += [err[trainsize][k]]
+                        state.besterrepoch[trainsize] += [k]
         else:
             state.bestrec +=[None]
             state.bestrecepoch += [None]
-            state.besterr100 += [None]
-            state.besterr100epoch += [None]
-            state.besterr1000 += [None]
-            state.besterr1000epoch += [None]
-            state.besterr10000 += [None]
-            state.besterr10000epoch += [None] 
+            for trainsize in VALIDATION_TRAININGSIZE:
+                state.besterr[trainsize] += [None]
+                state.besterrepoch[trainsize] += [None]
     return channel.COMPLETE
